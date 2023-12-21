eaf9fba9208bc33c85749752162a342fb767b95b
Kuldeep <kuldeepshukla09496@gmail.com>
2023-12-22 01:04:40 +0530

    solution - 02-jwt

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,21 +1,44 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const { z } = require("zod");
+
 const jwtPassword = "secret";
 
-function signJwt(username, password) {
+// Zod validation schema for JWT input
+const jwtInputSchema = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
 
+function signJwt(username, password) {
+  try {
+    const input = jwtInputSchema.parse({ username, password });
+    return jwt.sign({ username: input.username }, jwtPassword);
+  } catch (error) {
+    return null;
+  }
 }
 
 function verifyJwt(token) {
-
+  try {
+    jwt.verify(token, jwtPassword);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 function decodeJwt(token) {
-
+  try {
+    jwt.verify(token);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
 
 module.exports = {
-    signJwt,
-    verifyJwt,
-    decodeJwt,
-    jwtPassword
-}
+  signJwt,
+  verifyJwt,
+  decodeJwt,
+  jwtPassword,
+};
